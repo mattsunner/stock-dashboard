f1daeb777445bc6bf4189758abf84d9cd89afa38
Matthew Sunner <matt@mattsunner.com>
2021-02-09 21:26:40 -0600

    Environment Setup & Dependency Logging

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import re
+
+with open('README.md') as fp:
+    long_description = fp.read()
+
+
+def find_version():
+    with open('stock_visuals/__init__.py') as fp:
+        for line in fp:
+            match = re.search(r"__version__\s*=\s*'([^']+)'", line)
+            if match:
+                return match.group(1)
+    assert False, 'cannot find version'
+
+
+setup(
+    name='Stock Visuals',
+    version=find_version(),
+    author='Matthew Sunner',
+    author_email='matt@mattsunner.com',
+    license='LICENSE.txt',
+    description='Visualization functions built with Plotly',
+    long_description=long_description,
+)
